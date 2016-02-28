07b18edf3ee757987e16a94feb1b69c23ac83d46
qcampos <quentin.campos@gmail.com>
2016-02-28 16:17:42 +0100

    Changing the models and adding some test views.
    Those tests are motivated by the spatialite database we have to test at multiple development stages.

--- spike/backend/models.py
+++ spike/backend/models.py
@@ -6,7 +6,7 @@ from django.contrib.gis.db import models
 # Create your models here.
 
 class Alert(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
     lon = models.FloatField()
 
