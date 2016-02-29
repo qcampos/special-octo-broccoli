328b0fe61ce96db49646544fc817db615b5151c9
qcampos <quentin.campos@gmail.com>
2016-02-29 14:24:27 +0100

    Adding the administration view configuration at /admin in order to access and modify data more easily.

--- spike/backend/admin.py
+++ spike/backend/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from backend.models import User, Alert
+
 
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['pin']}),
+        ('User data', {'fields': ['first_name', 'last_name', 'phone']}),
+    ]
+    list_display = ['pin', 'first_name', 'last_name', 'id']
+    search_fields = ['pin', 'first_name', 'last_name']
+
+
+class AlertAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'author']}),
+        ('Position', {'fields': ['position'], 'classes': ['collapse']}),
+    ]
+    list_display = ['name', 'author', 'position', 'id']
+    search_fields = ['name', 'author']
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Alert, AlertAdmin)
