2b673118bafc91e13cb6017348494c1d24373693
Sylvestre Massalaz <sylvestre.massalaz@gmail.com>
2016-03-01 00:19:44 +0100

    Correctign the views to match the documentation

--- spike/backend/urls.py
+++ spike/backend/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     url(r'^users/register$', views.userRegister, name='userRegister'),
     url(r'^users/login$', views.userLogin, name='userLogin'),
+    url(r'^users/check/mail', views.userCheckEmail, name='userCheckEmail'),
+    url(r'^users/check/mail', views.userCheckPhone, name='userCheckPhone'),
     url(r'^alerts/getlist$', views.alertGetlist, name='alertGetlist'),
     url(r'^alerts/get$', views.alertGet, name='alertGet'),
     url(r'^alerts/add$', views.alertAdd, name='alertAdd'),
-    url(r'^alerts/update$', views.alertUpdate, name='alertUpdate'),
     url(r'^alerts/close$', views.alertClose, name='alertClose'),
-    url(r'^alerts/myalerts$', views.alertMyalerts, name='alertMyalerts'),
 ]
