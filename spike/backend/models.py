99b3c1fd7138146e951f292bb10f59081d372719
Sylvestre Massalaz <sylvestre.massalaz@gmail.com>
2016-02-29 21:12:04 +0100

    Adding email field to the user

--- spike/backend/models.py
+++ spike/backend/models.py
@@ -10,10 +10,11 @@ class User(models.Model):
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     pin = models.CharField(max_length=4)
-    phone = models.CharField("Phone number", max_length=12,
+    phone = models.CharField("Phone number", max_length=12, unique=True,
                              validators=[validators.RegexValidator(regex=r"^\+3{2}\d{9}|\d{10}$")])
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    mail = models.CharField(max_length=512, unique=True, validators=[validators.EmailValidator])
 
     def __str__(self):
         return "{} : {} {}".format(self.pin, self.first_name, self.last_name)
