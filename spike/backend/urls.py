80f71310a73c8db10f663a9573fe7b6179010c93
qcampos <quentin.campos@gmail.com>
2016-03-01 16:22:51 +0100

    Reorganising urls order.

--- spike/backend/urls.py
+++ spike/backend/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^users/register$', views.userRegister, name='userRegister'),
-    url(r'^users/login$', views.userLogin, name='userLogin'),
     url(r'^users/check/mail$', views.userCheckEmail, name='userCheckEmail'),
     url(r'^users/check/phone$', views.userCheckPhone, name='userCheckPhone'),
+
+    url(r'^users/register$', views.userRegister, name='userRegister'),
+    url(r'^users/login$', views.userLogin, name='userLogin'),
+
     url(r'^alerts/getlist$', views.alertGetlist, name='alertGetlist'),
     url(r'^alerts/get$', views.alertGet, name='alertGet'),
     url(r'^alerts/add$', views.alertAdd, name='alertAdd'),
